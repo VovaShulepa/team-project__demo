2e085c70be0bbbe4c5985293451785e9876e00d0
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-10-10 18:32:36 +0300

    commit

--- src/swiper/swiper-settings.js
+++ src/swiper/swiper-settings.js
@@ -4,9 +4,17 @@
 let swiper = new Swiper(".swiper", {
    loop: true,
    spaceBetween: 24,
-    grabCursor: true,
+   grabCursor: true,
+ 
+        autoplay: {
+          delay: 2300,
+          disableOnInteraction: false,
+  },
+        
         pagination: {
           el: ".swiper-pagination",
           dynamicBullets: true,
+          clickable: true,
         },
-      });
+});
+      
