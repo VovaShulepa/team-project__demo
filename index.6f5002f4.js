aca7d11522a424e97c79ed857d3a75e92a93d9a3
VovaShulepa <106019079+VovaShulepa@users.noreply.github.com>
2022-10-11 15:12:57 +0000

    Deploying to gh-pages from @ VovaShulepa/team-project__demo@087b62dc53841ff14274e17ddfb62c4ce8601578 🚀

---
+++ index.6f5002f4.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,spaceBetween:24,grabCursor:!0,speed:3e3,autoplay:{delay:4600,disableOnInteraction:!1},pagination:{el:".swiper-pagination",dynamicBullets:!0,clickable:!0}});
+//# sourceMappingURL=index.6f5002f4.js.map
--- index.9637fc54.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper",{loop:!0,spaceBetween:24,grabCursor:!0,pagination:{el:".swiper-pagination",dynamicBullets:!0}});
-//# sourceMappingURL=index.9637fc54.js.map
