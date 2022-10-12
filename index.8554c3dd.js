f9f51f0e343f08fe0332792518d2575e5186ccc8
VovaShulepa <106019079+VovaShulepa@users.noreply.github.com>
2022-10-12 19:17:04 +0000

    Deploying to gh-pages from @ VovaShulepa/team-project__demo@1899dda206af339194e41675d92878fce1769d63 🚀

--- index.8554c3dd.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.body,videoEl:document.querySelector(".about__video")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("overflow")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",(function(){o.videoEl.pause(),e()}))}();
-//# sourceMappingURL=index.8554c3dd.js.map
---
+++ index.fae0a06e.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.body,videoEl:document.querySelector(".about__video")};function e(){o.modal.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",(function(){o.videoEl.pause(),e()}))}();
+//# sourceMappingURL=index.fae0a06e.js.map
