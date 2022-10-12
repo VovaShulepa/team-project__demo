adaa32535b3e1db8d7aed0690af578631cdc6a54
VovaShulepa <106019079+VovaShulepa@users.noreply.github.com>
2022-10-12 08:31:51 +0000

    Deploying to gh-pages from @ VovaShulepa/team-project__demo@f0c8f1861f8a55b194439ee1e6c1e983cc74f25d 🚀

--- index.9f771d7a.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.body};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("overflow")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.9f771d7a.js.map
