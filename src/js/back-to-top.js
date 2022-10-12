a6b64d912f516beb83468f61ed1802eef84bdfe8
Maryna Naidych <maryna.naidych@gmail.com>
2022-10-12 21:05:48 +0300

    add button up

--- src/js/back-to-top.js
+++ src/js/back-to-top.js
@@ -1,18 +1,16 @@
-function backToTop() {
-let button = $('.back-to-top');
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-$(window).on('scroll',() => {
-    if ($(this).scrollTop() >= 50) {
-        button.fadeIn();
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
     } else {
-        button.fadeOut();
+        document.getElementById("myBtn").style.display = "none";
     }
-});
-
-button.on('click', (e) => {
-    e.preventDefault();
-    $('html').animate({scrollTop: 0}, 1000)
-})
 }
 
-backToTop();
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
