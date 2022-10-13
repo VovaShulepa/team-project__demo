127f6c3237a04bf548a14e8adf254dda8d9f5341
DmytroNadolynskyi <113470234+DmytroNadolynskyi@users.noreply.github.com>
2022-10-13 09:57:43 +0300

    modalfinish

--- src/js/modal-contacts.js
+++ src/js/modal-contacts.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-contacts-open]"),
     closeModalBtn: document.querySelector("[data-modal-contacts-close]"),
     modal: document.querySelector("[data-modal-contacts]"),
+    body:document.body
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+     refs.body.classList.toggle("overflow");
   }
 })();
